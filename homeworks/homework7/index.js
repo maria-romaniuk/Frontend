459e7fb928bd9640e2477b5ea5aeaf6c600eaadc
maria-romaniuk <romaniuk.mary@gmail.com>
2024-04-09 13:27:47 +0200

    homework8

---
+++ homeworks/homework7/index.js
@@ -0,0 +1,28 @@
+
+//-------Task1
+let a = true;
+let b = 23;
+let c = 'alocasia';
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+
+
+//------Task2
+
+let x= 34;
+let y = x;
+
+console.log(x);
+console.log(y);
+
+
+//-------Task3
+let arr = [1,2,3,4,5,6,12];
+
+arr.forEach(x => console.log(x));
+
+arr.slice().reverse().forEach(a=> console.log(a)); //creates a shallow copy, reverse and print with forEach
