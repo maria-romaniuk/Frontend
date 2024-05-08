a556e274ac2ccca9a0aaa4a50d9f052a1fd68d32
maria-romaniuk <romaniuk.mary@gmail.com>
2024-05-08 09:23:58 +0200

    extra2

--- extratraining/index.js
+++ extratraining/index.js
@@ -83,8 +83,21 @@ function min(list){
   }
 
   console.log(min(list));
-  
+
   const min = function(list){
     list.sort((a, b) => (a - b));
     return list[0];
+}
+
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: 
+//"1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+const countSheep = function (num){
+    let stringText = ''
+    for(let i = 1; i <= num; i++){
+      stringText += i + " sheep...";
+    }
+    return stringText; 
 }
