b5cd5ef10dce038656ac4f85e04f9253846499a9
maria-romaniuk <romaniuk.mary@gmail.com>
2024-05-23 11:16:18 +0200

    mongo_2

--- mongoDB/db.drinks.js
+++ mongoDB/db.drinks.js
@@ -1,16 +1,16 @@
-// use("drinks_db");
+use("drinks_db");
 
-// db.drinks.insertMany([
-//     {name:'MilkShake', price: 5, strong: 0, ml: 400},
-//     {name:'Smoosie', price: 8.5, strong: 0, ml: 450},
-//     {name:'Gin-Tonic', price: 8, strong: 1, ml: 200},
-//     {name:'Vine', price: 5, strong: 1, ml: 100},
-//     {name:'Whiskey Sour', price: 12, strong: 2, ml: 220},
-//     {name:'Caipirinha', price: 8, strong: 3, ml: 300},
-//     {name:'Aperol Spritz', price: 6.5, strong: 2, ml: 400},
-//   ])
+db.drinks.insertMany([
+    {name:'MilkShake', price: 5, strong: 0, ml: 400},
+    {name:'Smoosie', price: 8.5, strong: 0, ml: 450},
+    {name:'Gin-Tonic', price: 8, strong: 1, ml: 200},
+    {name:'Vine', price: 5, strong: 1, ml: 100},
+    {name:'Whiskey Sour', price: 12, strong: 2, ml: 220},
+    {name:'Caipirinha', price: 8, strong: 3, ml: 300},
+    {name:'Aperol Spritz', price: 6.5, strong: 2, ml: 400},
+  ])
 
-//   db.drinks.find().sort({name: -1})
+  db.drinks.find().sort({name: -1})
 
-// db.drinks.findOne({name: 'Caipirinha'});
-// db.drinks.find().limit(-2);
+db.drinks.findOne({name: 'Caipirinha'});
+db.drinks.find().limit(-2);
