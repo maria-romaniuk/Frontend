f936573fb6b865433f0e776e4827ce6a5a94d216
maria-romaniuk <romaniuk.mary@gmail.com>
2024-07-12 12:39:19 +0200

    quote_status

--- Frontend-prof/14_redux_09_07/src/components/Sandwich.tsx
+++ Frontend-prof/14_redux_09_07/src/components/Sandwich.tsx
@@ -1,25 +1,45 @@
 
 import { RootState } from '../reduxRTK/storeRTK';
 import { useDispatch, useSelector } from 'react-redux';
-import { addIngredient, clearAll } from '../reduxRTK/sandwichSlice';
+import { addIngredient, clearAll, deleteLast, fetchQuote } from '../reduxRTK/sandwichSlice';
+import { useEffect } from 'react';
 
 export const Sandwich = () => {
-    const sandwich = useSelector((state: RootState) => state.sandwich.value);
-    const dispatch = useDispatch();
+  const {value, quote, status} = useSelector((state: RootState) => state.sandwich);
+  const dispatch = useDispatch();
 
+
+  useEffect(() =>{
+     dispatch(fetchQuote())
+  }, [])
   return (
     <div className='container d-flex flex-column align-items-center p-5'>
-        <h2>Choose your ingredients:</h2>
-        <div className='btns'>
-            {/* <button className='btn btn-info' onClick={() => dispatch({type: 'addIngredient', payload: 'bread'})}>Bread</button> */}
-            <button className='btn btn-info' onClick={() => dispatch(addIngredient('bread') )}>Bread</button>
-            <button className='btn btn-info mx-3' onClick={() => dispatch(addIngredient( 'salad'))}>Salad</button>
-            <button className='btn btn-info' onClick={() => dispatch(addIngredient( 'ham'))}>Ham</button>
-            <button className='btn btn-info mx-3' onClick={() => dispatch(addIngredient( 'tomato'))}>Tomato</button>
-            <button className='btn btn-info' onClick={() => dispatch(addIngredient( 'cheese'))}>Cheese</button>
-        </div>
-        <div className='m-3' style={{backgroundColor: "rgb(255, 255, 255)", borderRadius: "4px", fontSize:" 22px", padding: '15px'}}>Your order: <strong> {sandwich}</strong></div>
-        <div><button  className='btn btn-info' onClick={() => dispatch(clearAll())}>Clear all</button></div>
+      { status === 'loading ' && (
+        <div className="spinner-border" role="status">
+        <span className="sr-only"></span>
+      </div>
+      )}
+      {status === 'succeeded' && (
+        <div>{quote?.sentence}</div>
+      )}
+      {status === 'failed' && (
+        <div>fetch failed</div>
+      )}
+
+      <h2>Choose your ingredients:</h2>
+      <div className='btns'>
+        {/* <button className='btn btn-info' onClick={() => dispatch({type: 'addIngredient', payload: 'bread'})}>Bread</button> */}
+        <button className='btn btn-info' onClick={() => dispatch(addIngredient('bread'))}>Bread</button>
+        <button className='btn btn-info mx-3' onClick={() => dispatch(addIngredient('salad'))}>Salad</button>
+        <button className='btn btn-info' onClick={() => dispatch(addIngredient('ham'))}>Ham</button>
+        <button className='btn btn-info mx-3' onClick={() => dispatch(addIngredient('tomato'))}>Tomato</button>
+        <button className='btn btn-info' onClick={() => dispatch(addIngredient('cheese'))}>Cheese</button>
+      </div>
+      <div className='m-3' style={{ backgroundColor: "rgb(255, 255, 255)", borderRadius: "4px", fontSize: " 22px", padding: '15px' }}>Your order: <strong> {value}</strong></div>
+      <div>
+        <button className='btn btn-info mr-3' onClick={() => dispatch(deleteLast())}>Delete last</button>
+        <button className='btn btn-info ml-3' onClick={() => dispatch(clearAll())}>Clear all</button>
+      </div>
     </div>
   )
 }
--- Frontend-prof/14_redux_09_07/src/reduxRTK/sandwichSlice.ts
+++ Frontend-prof/14_redux_09_07/src/reduxRTK/sandwichSlice.ts
@@ -1,22 +1,64 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
 
 export interface InitialStateS {
     value: string;
+    quote: IQuote | null;
+    status: 'loading '| 'succeeded' | 'failed' | null;
 }
 
+interface IQuote{
+    sentence: string,
+    character:{
+        name: string,
+        slug: string,
+        house:{
+            name: string,
+            slug: string
+        }
+    }
+}
 const initialState: InitialStateS = {
     value: "",
+    quote: null,
+    status: null,
 }
+
+export const fetchQuote = createAsyncThunk<IQuote>('fetchQuote', async () => {
+    const response = fetch('https://api.gameofthronesquotes.xyz/v1/random')
+        const data = (await response).json();
+    return data;
+})
 
 const sandwichSlice = createSlice({
     name: 'sandwich',
     initialState,
     reducers: {
-        addIngredient(state, action: PayloadAction<string>){ state.value += " " + action.payload},
-        clearAll(state){state.value = ""}
+        addIngredient(state, action: PayloadAction<string>) { state.value += " " + action.payload },
+        clearAll(state) { state.value = "" },
+        deleteLast(state) {
+            const ingredients = state.value.trim().split(" ");
+            ingredients.pop();
+            state.value = ingredients.join(" ");
+        } //объединяем в массив, отрезаем последний и объединяем массив обратно в строку
+    },
+    extraReducers(builder) {
+        builder
+            .addCase(fetchQuote.pending, (state) => {
+                //Loading
+                state.status = 'loading '
+            })
+            .addCase(fetchQuote.fulfilled, (state, action) => {
+                //когда успешная загрузка произошла
+                state.status = 'succeeded'
+                state.quote = action.payload
+            })
+            .addCase(fetchQuote.rejected, (state) => {
+                state.status = 'failed'
+            })
     }
 })
 
-export const { addIngredient, clearAll } = sandwichSlice.actions
+export const { addIngredient, clearAll, deleteLast } = sandwichSlice.actions
 
 export default sandwichSlice.reducer
